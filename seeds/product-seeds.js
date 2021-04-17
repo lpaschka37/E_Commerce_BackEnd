a850f48985c647623ebf9bdab5adc1d837ba45e8
Luke Paschka <lpaschka37@gmail.com>
2021-04-17 10:23:22 -0500

    updates to seed file again

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -31,6 +31,12 @@ const productData = [
     stock: 22,
     category_id: 2,
   },
+  {
+    product_name: 'Shorts',
+    price: 19.99,
+    stock: 20,
+    category_id: 2,
+  },
 ];
 
 const seedProducts = () => Product.bulkCreate(productData);
